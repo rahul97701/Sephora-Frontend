0cf3dfa0a464bad9da8310bb04ddf405c978e309
rahulkhichi <singh.rahulsingh.om07@gmail.com>
2024-08-13 08:05:07 -0400

    "Changes done"

--- src/Routers/AllRoutes.jsx
+++ src/Routers/AllRoutes.jsx
@@ -1,30 +1,26 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "../pages/Home";
-import Header from "../components/Navbar/Header";
-import Navbar1 from "../components/Navbar/Navbar1";
-// import CartPage from "../pages/CartPage";
-// import PrivateRoutes from "./PrivateRoutes";
-// import Products from "../pages/Products";
-// import Product from "../pages/Product";
+  import { Route, Routes } from "react-router-dom";
+  import Home from "../pages/Home";
+  import CartPage from "../pages/CartPage";
+  import PrivateRoutes from "./PrivateRoutes";
+  import Products from "../pages/Products";
+  import Product from "../pages/Product";
 
-const AllRoutes = () => {
-  return (
-    <Routes>
-      {/* <Route path="/home" element={<Home />} />
-      <Route path="/header" element={<Header />} /> */}
-      <Route path="/Navbar" element={<Navbar1 />} />
-      {/* <Route
-        path="/cart"
-        element={
-          <PrivateRoutes>
-            <CartPage />
-          </PrivateRoutes>
-        }
-      />
-      <Route path="/products" element={<Products />} />
-      <Route path="/products/:id" element={<Product />} /> */}
-    </Routes>
-  );
-};
+  const AllRoutes = () => {
+    return (
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route
+          path="/cart"
+          element={
+            <PrivateRoutes>
+              <CartPage />
+            </PrivateRoutes>
+          }
+        />
+        <Route path="/products" element={<Products />} /> 
+        <Route path="/products/:id" element={<Product />} />
+      </Routes>
+    );
+  };
 
-export default AllRoutes;
+  export default AllRoutes;
