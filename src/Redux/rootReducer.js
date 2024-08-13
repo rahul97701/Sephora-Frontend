e6f06cc08602f6f46a9c9e29c3c3cbc78f15f331
rahulkhichi <singh.rahulsingh.om07@gmail.com>
2024-08-13 08:02:16 -0400

    "Changes done"

--- src/Redux/rootReducer.js
+++ src/Redux/rootReducer.js
@@ -1,8 +1,13 @@
-import { combineReducers } from "redux";
+    import { combineReducers } from "redux";
+    import { loginReducer } from "./Login/reducer";
+    import { cartReducer } from "./Cart/reducer";
+    import { loadingReducer } from "./Loading/reducer";
 
 
-const rootReducer = combineReducers({
+        const rootReducer = combineReducers({
+        loginState: loginReducer,
+        cartData: cartReducer,
+        loading: loadingReducer,
+        });
 
-});
-
-export default rootReducer;
+        export default rootReducer;
